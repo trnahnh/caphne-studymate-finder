a74f43a678432ee7096e862dd4cb8297e7920877
khiem <khiemnguyen.hye@gmail.com>
2026-01-12 13:31:38 +0700

    Preserve more work

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -1,5 +1,18 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: './app/components/ui'
+  },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins:[
+      tailwindcss(),
+    ]
+  }
 })

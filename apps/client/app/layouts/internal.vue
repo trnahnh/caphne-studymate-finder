dda313b1fc3c9bdd890c0842576c4fee62e05138
khiem <khiemnguyen.hye@gmail.com>
2026-01-15 22:36:03 +0700

    Scaffold start page

---
+++ apps/client/app/layouts/internal.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="dark min-h-screen bg-background text-foreground">
+    <main class="mx-auto max-w-7xl">
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ apps/client/app/pages/start.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({ layout: "internal" })
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <h1>Are you a?</h1>
+    <Button>
+      A guy
+    </Button>
+  </div>
+</template>

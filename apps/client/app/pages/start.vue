c4fcbaebdc7e7630ea604555a5399eae962a58ff
khiem <khiemnguyen.hye@gmail.com>
2026-01-17 16:49:25 +0700

    Add user preferences

--- apps/client/app/pages/start.vue
+++ apps/client/app/pages/start.vue
@@ -3,12 +3,14 @@ definePageMeta({ layout: "internal" })
 
 type Gender = "male" | "female" | "other";
 type Vibe = "antara" | "owl" | "quinx";
+type Preference = "Lesseo" | "Rei" | "Gaeul" | "Yujin"
 
 const currentQuestion = ref(1);
 const selectedGender = ref<Gender>();
 const selectedVibe = ref<Vibe>();
+const selectedPreference = ref<Preference>();
 
-const totalQuestions = 2;
+const totalQuestions = 3;
 
 const onNext = () => {
   if (currentQuestion.value < totalQuestions) {
@@ -24,9 +26,10 @@ const onPrevious = () => {
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center h-screen gap-6">
+  <div class="flex justify-center items-center h-screen">
     <!-- Question 1 -->
-    <div v-if="currentQuestion === 1">
+    <div v-if="currentQuestion === 1" class="flex flex-col gap-6 justify-center items-center">
+      <h1>Your gender is...</h1>
       <div class="flex flex-col gap-2 justify-center">
         <div class="flex justify-center gap-2">
           <Button class="hover:px-6" @click="selectedGender = 'male'">Male</Button>
@@ -36,25 +39,30 @@ const onPrevious = () => {
       </div>
     </div>
     <!-- Question 2 -->
-    <div class="flex flex-col gap-2 justify-center">
-    <div v-if="currentQuestion === 2">
-        <div class="flex justify-center gap-2">
-          <Button class="hover:px-6" @click="selectedVibe = 'antara'">Male</Button>
-          <Button class="hover:px-6" @click="selectedVibe = 'owl'">Owl</Button>
-          <Button class="hover:px-6" @click="selectedVibe = 'quinx'">Quinx</Button>
-          <Button variant="outline" class="hover:px-6" @click="selectedGender = 'other'">Other</Button>
-        </div>
+    <div v-if="currentQuestion === 2" class="flex flex-col gap-6 justify-center items-center">
+      <h1>Your vibe is...</h1>
+      <div class="flex justify-center gap-2">
+        <Button class="hover:px-6" @click="selectedVibe = 'antara'">Antara</Button>
+        <Button class="hover:px-6" @click="selectedVibe = 'quinx'">Quinx</Button>
+        <Button class="hover:px-6" @click="selectedVibe = 'owl'">Owl</Button>
+        <Button variant="outline" class="hover:px-6" @click="selectedGender = 'other'">Other</Button>
       </div>
     </div>
-    <h1>Your gender is...</h1>
-    <div>
-      Selected: {{ selectedGender }}
+    <!-- Question 3 -->
+    <div v-if="currentQuestion === 3" class="flex flex-col gap-6 justify-center items-center">
+      <h1>You fuck with...</h1>
+      <div class="flex justify-center gap-2">
+        <Button class="hover:px-6" @click="selectedPreference = 'Lesseo'">Lesseo</Button>
+        <Button class="hover:px-6" @click="selectedPreference = 'Rei'">Rei</Button>
+        <Button class="hover:px-6" @click="selectedPreference = 'Yujin'">Yujin</Button>
+        <Button class="hover:px-6" @click="selectedPreference = 'Gaeul'">Gaeul</Button>
+        <Button variant="outline" class="hover:px-6" @click="selectedGender = 'other'">Other</Button>
+      </div>
     </div>
   </div>
-  <ProgressControl 
+  <ProgressControl
     :currentQuestion="currentQuestion"
     :totalQuestions="totalQuestions"
     :onNext="onNext"
-    :onPrevious="onPrevious"
-  />
+    :onPrevious="onPrevious" />
 </template>

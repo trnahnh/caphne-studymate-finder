f4c6719b347dded0ad6cd04d7a0bf28777e1ad74
khiem <khiemnguyen.hye@gmail.com>
2026-01-23 23:45:59 +0700

    Add textarea

--- apps/client/app/pages/start.vue
+++ apps/client/app/pages/start.vue
@@ -12,6 +12,15 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover'
 
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSet,
+} from '@/components/ui/field'
+import { Textarea } from '@/components/ui/textarea'
+
 definePageMeta({ layout: "internal" })
 
 type Gender = "male" | "female" | "other";
@@ -52,7 +61,7 @@ const df = new DateFormatter('en-GB', {
     <!-- Question 1 -->
     <div v-if="currentQuestion === 1" class="flex flex-col gap-10 justify-center items-center">
       <h1 class="text-muted-foreground flex justify-center items-center hover:text-foreground transition-all">
-        <Icon name="streamline-pixel:food-drink-desert-cake" size="23" class="mr-2"/>
+        <Icon name="streamline-pixel:food-drink-desert-cake" size="23" class="mr-2" />
         Let's get you setup
       </h1>
       <div class="flex flex-col gap-4 items-center">
@@ -61,15 +70,15 @@ const df = new DateFormatter('en-GB', {
           <div class="flex justify-center gap-2">
             <Button variant="outline" :class="`size-17 ${selectedGender === 'male' && 'bg-primary/60'}`"
               @click="selectedGender = 'male'">
-              <Icon name="streamline-pixel:user-gender-male" size="40"/>
+              <Icon name="streamline-pixel:user-gender-male" size="40" />
             </Button>
             <Button variant="outline" :class="`size-17 {selectedGender === 'female' && 'bg-primary/60'}`"
               @click="selectedGender = 'female'">
-              <Icon name="streamline-pixel:user-gender-female" size="40"/>
+              <Icon name="streamline-pixel:user-gender-female" size="40" />
             </Button>
             <Button variant="outline" :class="`size-17 ${selectedGender === 'other' && ''}`"
               @click="selectedGender = 'other'">
-              <Icon name="pixel:face-thinking-solid" size="40"/>
+              <Icon name="pixel:face-thinking-solid" size="40" />
             </Button>
           </div>
         </div>
@@ -83,12 +92,12 @@ const df = new DateFormatter('en-GB', {
               !date && 'text-muted-foreground',
             )">
               <CalendarIcon class="mr-2 h-4 w-4" />
-              {{ date ? df.format(date.toDate(getLocalTimeZone())) : "dd/mm/yyyy" }}
+              {{ date ? df.format(date.toDate(getLocalTimeZone())) : "dd/MM/YYYY" }}
             </Button>
           </PopoverTrigger>
           <PopoverContent class="w-auto p-0">
             <Calendar v-model="date" :initial-focus="true" :default-placeholder="defaultPlaceholder"
-            layout="month-and-year" />
+              layout="month-and-year" />
           </PopoverContent>
         </Popover>
       </div>
@@ -96,7 +105,7 @@ const df = new DateFormatter('en-GB', {
     <!-- Question 2 -->
     <div v-if="currentQuestion === 2" class="flex flex-col gap-10 justify-start items-start">
       <h1 class="text-muted-foreground flex justify-center items-center hover:text-foreground transition-all">
-        <Icon name="streamline-pixel:interface-essential-information-circle-1" size="23" class="mr-2"/>
+        <Icon name="streamline-pixel:interface-essential-information-circle-1" size="23" class="mr-2" />
         Some more basic info
       </h1>
       <div class="flex flex-col gap-2 justify-start items-start">
@@ -160,7 +169,7 @@ const df = new DateFormatter('en-GB', {
     <!-- Question 3 -->
     <div v-if="currentQuestion === 3" class="flex flex-col gap-6 justify-center items-center">
       <h1 class="text-muted-foreground flex justify-center items-center hover:text-foreground transition-all">
-        <Icon name="streamline-pixel:interface-essential-question-help-square" size="23" class="mr-2"/>
+        <Icon name="streamline-pixel:interface-essential-question-help-square" size="23" class="mr-2" />
         What brings you here?
       </h1>
       <div class="flex flex-col gap-2">
@@ -169,8 +178,8 @@ const df = new DateFormatter('en-GB', {
             @click="selectedVibe = 'antara'">Find study buddy</Button>
           <Button :class="`hover:px-6 ${selectedVibe === 'quinx' && 'bg-primary/60'}`"
             @click="selectedVibe = 'quinx'">Find project teammate</Button>
-          <Button :class="`hover:px-6 ${selectedVibe === 'owl' && 'bg-primary/60'}`"
-            @click="selectedVibe = 'owl'">Learn new stuff</Button>
+          <Button :class="`hover:px-6 ${selectedVibe === 'owl' && 'bg-primary/60'}`" @click="selectedVibe = 'owl'">Learn
+            new stuff</Button>
         </div>
         <div class="flex justify-center gap-2">
           <Button :class="`hover:px-6 ${selectedVibe === 'antara' && 'bg-primary/60'}`"
@@ -195,10 +204,22 @@ const df = new DateFormatter('en-GB', {
       </div>
     </div>
     <!-- Question 5 -->
-    <div v-if="currentQuestion === 5" class="flex flex-col gap-6 justify-center items-center">
-      <h1>A short bio to introduce yourself?</h1>
-      <div class="flex justify-center gap-2">
-        <Input type="text" placeholder="I like to video call and work..." class="w-sm h-15" />
+    <div v-if="currentQuestion === 5" class="flex flex-col justify-center items-center">
+      <h1>Upload your profile picture</h1>
+      <Card class="border border-dashed size-20">
+        <CardContent>
+          <Icon name="mdi:account" size="32" />
+        </CardContent>
+      </Card>
+      <h1>A short bio to introduce yourself</h1>
+      <div class="w-sm">
+        <FieldSet>
+          <FieldGroup>
+            <Field>
+              <Textarea id="feedback" placeholder="I like a bit of League..." :rows="4" />
+            </Field>
+          </FieldGroup>
+        </FieldSet>
       </div>
     </div>
     <!-- Question 6 -->

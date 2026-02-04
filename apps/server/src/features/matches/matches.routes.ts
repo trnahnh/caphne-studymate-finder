ecf4027c08c561a39f0540b3aa10abdfc7594bfd
khiem <khiemnguyen.hye@gmail.com>
2026-02-04 12:01:03 +0700

    Add match returning mechanism

--- apps/server/src/features/matches/matches.routes.ts
+++ apps/server/src/features/matches/matches.routes.ts
@@ -1,27 +1,28 @@
 import { Router } from "express";
-import { generateNewMatch } from "./matches.services.js";
+import { generateNewMatch, getAllMatches } from "./matches.services.js";
 import { requireAuth } from "../../middleware/requireAuth.js";
 import { User } from "../../db/schema.js";
 
 export const matchesRouter = Router()
 
-matchesRouter.get('/new', requireAuth, async (req, res) => {
+matchesRouter.post('/', requireAuth, async (req, res) => {
   const user = req.user as User
 
   try {
     const match = await generateNewMatch(user.id)
     res.json({ match: match })
   } catch (e) {
-    console.log(`Error getting new matches: ${e}`)
+    console.log(`Error generating new matches: ${e}`)
     res.json({ error: 'Something went wrong' })
   }
 })
 
-matchesRouter.get('/all', requireAuth, async (req, res) => {
+matchesRouter.get('/', requireAuth, async (req, res) => {
   const user = req.user as User
 
   try {
-    
+    const allMatches = await getAllMatches(user.id)
+    res.json({matches: allMatches})
   } catch (e) {
     console.log(`Error getting all matches: ${e}`)
     res.json({ error: 'Something went wrong' })
--- apps/server/src/features/matches/matches.services.ts
+++ apps/server/src/features/matches/matches.services.ts
@@ -6,18 +6,34 @@ export const generateNewMatch = async (userId: number) => {
   const [match] = await db
     .select()
     .from(users)
-    .limit(1)
     .where(ne(users.id, userId))
+    .limit(1)
 
   if (!match) {
     throw new Error(`no matches found`)
   }
 
-  await db.insert(matches).values({
+  const [inserted] = await db
+    .insert(matches)
+    .values({
     userId: userId,
     matchedUserId: match.id
-  })
+  }).returning()
 
   console.log(`Successfully selected 1 match: ${match}`)
-  return match
+  return inserted
+}
+
+export const getAllMatches = async (userId: number) => {
+  const userMatches = await db
+    .select({
+      id: users.id,
+      email: users.email,
+      createdAt: users.createdAt
+    })
+    .from(matches)
+    .innerJoin(users, eq(matches.matchedUserId, users.id))
+    .where(eq(matches.userId, userId))
+
+  return userMatches
 }
